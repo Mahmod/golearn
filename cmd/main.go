9396473d83bdcc5ba46578a86c46778a1dd288dc
Mahmoud Elsherif <moelsherif@google.com>
2023-10-23 08:43:18 +0000

    complete internal user log

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
     "log"
     "github.com/golang/protobuf/proto"
     clientanalytics "metrics/log/event"
+	internal_user_log "metrics/log/atest"
 	"time"
 )
 
@@ -12,7 +13,7 @@ const (
 	//TODO: change it to appropriate clearcut value
 	ClientTypeValue   = 1
 	LogSourceValue    = 2
-	LogSourceNameValue = "cvdr"
+	LogSourceNameValue = "cuttlefish"
 )
 
 func currentTimeMillis() int64 {
@@ -42,8 +43,55 @@ func encodeLogRequest(extension string) ([]byte, error) {
 
 	return proto.Marshal(req)
 }
+
+const (
+	UserKey       = "some-user-key"
+	RunID         = "some-run-id"
+	ToolName      = "cvdr"
+	CommandLine   = "some-command"
+	Cwd           = "some-directory"
+	Os            = "Linux"
+)
+
+var (
+	UserType        = internal_user_log.UserType_GOOGLE
+	TestReferences  = []string{"test1", "test2"}
+)
+
+
+func encodeAtestLogEventInternal(logEvent *internal_user_log.AtestLogEventInternal) ([]byte, error) {
+	return proto.Marshal(logEvent)
+}
+
+func createAtestLogEventInternal() *internal_user_log.AtestLogEventInternal {
+	atestStartEvent := &internal_user_log.AtestLogEventInternal_AtestStartEvent{
+		CommandLine:    proto.String(CommandLine),
+		TestReferences: TestReferences,
+		Cwd:            proto.String(Cwd),
+		Os:             proto.String(Os),
+	}
+
+	return &internal_user_log.AtestLogEventInternal{
+		UserKey:         proto.String(UserKey),
+		RunId:           proto.String(RunID),
+		UserType:        &UserType,
+		ToolName:        proto.String(ToolName),
+		Event:           &internal_user_log.AtestLogEventInternal_AtestStartEvent_{AtestStartEvent: atestStartEvent},
+	}
+}
+
 func main() {
+	log.Printf("-----------------------------------")
 	log.Printf("Welcome to the Cuttlefish Metrics!")
+	logEvent := createAtestLogEventInternal()
+	encoded, err := encodeAtestLogEventInternal(logEvent)
+	if err != nil {
+		log.Fatalf("Failed to encode log event: %v", err)
+	}
+	log.Printf("Encoded Log Event: %v", encoded)
+
+	log.Printf("-----------------------------------")
+
     extension := "source_extension"
     data, err := encodeLogRequest(extension)
     if err != nil {
